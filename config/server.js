bdbe3481998edfb63cabe3db55c8dfc721748dd8
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-05-21 22:45:28 +0530

    civsa project inittiate

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://1ebf-2409-4072-15-9951-d8e1-e48f-4022-ecd.ngrok-free.app",
   app: {
     keys: env.array('APP_KEYS'),
   },
