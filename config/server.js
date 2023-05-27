7347d7a2e6324b7ba0d1a4d1934db1f430f5f007
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-05-27 11:00:46 +0530

    changes made

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://civsaecom.onrender.com",
+  //url: "https://civsaecom.onrender.com",
   app: {
     keys: env.array("APP_KEYS"),
   },
